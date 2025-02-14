e6f9ad33c77da223ceabbc1672aa032def011260
Danilo Costa <danilodalessandro08@gmail.com>
2025-02-14 13:54:22 -0300

    adjustment components

--- front/src/app/(pages)/centros/page.jsx
+++ front/src/app/(pages)/centros/page.jsx
@@ -1,13 +1,18 @@
-export default () => {
-    
-    return (
-        <div>
-        <h1>Centros</h1>   
-       
-        
-       
+"use client";
 
-        
-        </div>
-    );
+import { DataTable } from "@/components/core/table/data-table";
+import { columns } from "@/app/(pages)/centros/columns";
+
+const data = [
+  { status: "Active", email: "user1@example.com", amount: 100 },
+  { status: "Inactive", email: "user2@example.com", amount: 200 },
+];
+
+export default function CentrosPage() {
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-2xl font-bold mb-4">Centros</h1>
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
 }
