24b469f05fadc2c67fe00b43f202d6f9b91dbe91
Danilo Costa <danilodalessandro08@gmail.com>
2025-02-14 21:23:19 -0300

    including filters

--- front/src/app/(pages)/centros/columns.jsx
+++ front/src/app/(pages)/centros/columns.jsx
@@ -2,6 +2,7 @@
 
 import { MoreHorizontal } from "lucide-react";
 import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react"
 
 import { Button } from "@/components/ui/button";
 import {
@@ -20,7 +21,17 @@ export const columns = [
   },
   {
     accessorKey: "email",
-    header: "Email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
   },
   {
     accessorKey: "amount",
